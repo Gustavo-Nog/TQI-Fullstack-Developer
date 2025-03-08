65d94b87b7babf7eb8bf578f6fcf6c92c6099564
Gustavo-Nog <Gustavopan3g7@gmail.com>
2025-03-08 17:17:31 -0300

    Reorganização do repositório: removidos arquivos antigos e adicionados novos módulos HTML e JavaScript.

---
+++ Módulo_Javascript/Sintaxe_Básica/Variaveis_e_Valores/assets/js/script.js
@@ -0,0 +1,101 @@
+// TIPOS PRIMITIVOS
+
+// boolean
+/* var nameVariavel = false;
+console.log(typeof(nameVariavel));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nomeString = 'Javascript';
+console.log(typeof(nomeString)); 
+
+// function 
+var funcao = function() {}
+console.log(typeof(funcao)); */
+
+/* // COMO DECLARAR
+var variavel = "Javascript";
+variavel = "Js";
+console.log(variavel); 
+
+let variavel2 = "Javascript";
+variavel2 = "Js";
+console.log(variavel2);
+
+const variavel3 = "Js";
+constante = "Javascript"
+console.log(constante); */
+
+/* var escopoGlobal = "Global";
+console.log(escopoGlobal);
+
+function escopoLocal() {
+  let escopoLocalinterno = 'local';
+  console.log(escopoLocalinterno);
+}
+escopoLocal(); */
+
+/*  // ATRIBUIÇÃO
+var atribuicao = "javascript";
+
+// COMPARAÇÃO
+var comparacao = "0" == 0;
+console.log(comparacao);
+
+// COMPARAÇÃO IDÊNTICA
+var comparacaoIdentica = "0" === 0;
+console.log(comparacaoIdentica); */
+
+/* // adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtração
+var subtracao = 1 - 1;
+console.log(subtracao);
+
+// multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisão real
+var divisao = 6 / 2;
+console.log(divisao);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciação
+var potenciacao = 2 ** 10;
+console.logpotenciacao); */
+
+/* // OPERADORES RELACIONAIS
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual que
+var maiorIgualQue = 5 >= 2;
+console.log(maiorIgualQue);
+
+// menor ou igual que 
+var menorIgualQue = 5 <= 2;
+console.log(menorIgualQue); */
+
+/* // OPERADORES LÓGICOS
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao); */
---
+++ Módulo_Javascript/Sintaxe_Básica/Vetores_e_Objetos/assets/js/script.js
@@ -0,0 +1,32 @@
+// O QUE SÃO VETORES E ARRAYS
+
+/* // como declarar um array
+let array = ['string', 1, true];
+console.log(array); */
+
+// pode guardar vários tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+/* console.log(array[0]);
+
+// MANIPULANDO ARRAYS
+// forEach
+array.forEach(function(item, index) {console.log(item, index)});
+
+// push
+array.push('novo item');
+console.log(array);
+
+// pop
+array.pop();
+console.log(array) 
+
+// shift
+array.shift();
+console.log(array); 
+
+// unshift
+array.unshift('novo item no inicio.');
+console.log(array); 
+
+// indexOf
+console.log(array.indexOf(true)); */
