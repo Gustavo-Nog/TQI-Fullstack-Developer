d126dc4bae9eb3b24d021f489acd7bee7388594b
Gustavo-Nog <Gustavopan3g7@gmail.com>
2025-03-10 18:52:39 -0300

    Adiciona códigos do módulo JavaScript: Sintaxe Básica

---
+++ Módulo_Javascript/Sintaxe_Básica/Funcoes/assets/js/script.js
@@ -0,0 +1,21 @@
+/* // FUNÇÕES & SUAS PARTICULARIEDADES
+function funcao() {
+  console.log("essa mensagem veio de uma função :)");
+}
+funcao();
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo)
+}
+mensagem("Tudo certo jovem!", 5); 
+
+var funcao = function funcao() {
+  console.log("Sou uma mensagem de função de expressão")
+}
+funcao(); 
+
+// arrow fuction
+var funcao = () => {
+  console.log("Sou uma arrow function!");
+}
+funcao(); */
--- Módulo_Javascript/Sintaxe_Básica/Vetores_e_Objetos/assets/js/script.js
+++ Módulo_Javascript/Sintaxe_Básica/Vetores_e_Objetos/assets/js/script.js
@@ -2,11 +2,11 @@
 
 /* // como declarar um array
 let array = ['string', 1, true];
-console.log(array); */
+console.log(array);
 
 // pode guardar vários tipos de dados
-let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
-/* console.log(array[0]);
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']]; 
+console.log(array[0]);
 
 // MANIPULANDO ARRAYS
 // forEach
@@ -29,4 +29,24 @@ array.unshift('novo item no inicio.');
 console.log(array); 
 
 // indexOf
-console.log(array.indexOf(true)); */
+console.log(array.indexOf(true)); 
+
+array.splice(0, 3);
+console.log(array); 
+
+let novoArray =  array.slice(0, 3);
+console.log(novoArray); */
+
+/* // OBJETOS
+let object = {string: 'string', number: 1, boolean: true, array: ["array"], objectInterno: 
+  {objectInterno: 'objeto interno'}} 
+console.log(object.objectInterno);
+
+var string = object.string;
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno); 
+
+var {string, boolean, objectInterno} = object;
+console.log(string, boolean, objectInterno); */
